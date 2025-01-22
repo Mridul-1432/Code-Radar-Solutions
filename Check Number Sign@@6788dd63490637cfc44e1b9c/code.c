3e011c9a8f692d8cb8863aeafe950ae26c06f910
Mridul-1432 <mridul1432.becse24@chitkara.edu.in>
2025-01-22 13:35:36 +0530

    Solution to Check Number Sign

---
+++ Check Number Sign@@6788dd63490637cfc44e1b9c/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a>0){
+        printf("Positive");
+    }
+    else if(a<0){
+        printdf("Negative");
+    }
+    else{
+        print("Zero");
+    }
+    return 0;
+}
